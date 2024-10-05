3fb50858686785b3b64ce069621c4b5ccc6e64ff
Derrick Hammer <derrick@derrickhammer.com>
2024-10-05 01:32:15 -0400

    refactor: improve Reprovider efficiency and responsiveness
    - Separate main loop and trigger handling
    - Add mutex for thread-safe sleep duration management
    - Implement delayed trigger mechanism
    - Refactor provide logic into separate function
    - Adjust sleep durations based on provide results
    - Improve error handling and logging

--- internal/protocol/ipfs/provide.go
+++ internal/protocol/ipfs/provide.go
@@ -2,6 +2,7 @@ package ipfs
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/ipfs/boxo/provider"
@@ -36,7 +37,11 @@ type Reprovider struct {
 	store    ReprovideStore
 	log      *zap.Logger
 
-	triggerProvide chan struct{}
+	triggerProvide       chan struct{}
+	triggerDelayDuration time.Duration
+
+	mu             sync.Mutex
+	reprovideSleep time.Duration
 }
 
 // Trigger triggers the reprovider loop to run immediately.
@@ -50,8 +55,6 @@ func (r *Reprovider) Trigger() {
 // Run starts the reprovider loop, which periodically announces CIDs that
 // have not been announced in the last interval.
 func (r *Reprovider) Run(ctx context.Context, interval, timeout time.Duration, batchSize int) {
-	var reprovideSleep time.Duration
-
 	for {
 		if r.provider.Ready() {
 			break
@@ -60,83 +63,114 @@ func (r *Reprovider) Run(ctx context.Context, interval, timeout time.Duration, b
 		time.Sleep(30 * time.Second)
 	}
 
-	for {
-		r.log.Debug("sleeping until next reprovide time", zap.Duration("duration", reprovideSleep))
+	go r.handleTriggers(ctx, interval, timeout, batchSize)
 
-		triggered := false
+	for {
+		r.mu.Lock()
+		sleepDuration := r.reprovideSleep
+		r.mu.Unlock()
+
+		r.log.Debug("sleeping until next reprovide time", zap.Duration("duration", sleepDuration))
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(sleepDuration):
+			r.log.Debug("reprovide sleep expired")
+			nextSleep := r.performProvide(ctx, interval, timeout, batchSize)
+			r.mu.Lock()
+			r.reprovideSleep = nextSleep
+			r.mu.Unlock()
+		}
+	}
+}
+
+func (r *Reprovider) handleTriggers(ctx context.Context, interval, timeout time.Duration, batchSize int) {
+	var triggerTimer *time.Timer
+	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-r.triggerProvide:
-			triggered = true
+			if triggerTimer != nil {
+				triggerTimer.Stop()
+			}
+			triggerTimer = time.AfterFunc(r.triggerDelayDuration, func() {
+				r.performProvide(ctx, interval, timeout, batchSize)
+			})
 			r.log.Debug("reprovide triggered")
-		case <-time.After(reprovideSleep):
-			r.log.Debug("reprovide sleep expired")
-		}
-
-		doProvide := func(ctx context.Context, keys []multihash.Multihash) error {
-			ctx, cancel := context.WithTimeout(ctx, timeout)
-			defer cancel()
-			return r.provider.ProvideMany(ctx, keys)
-		}
-
-		for {
-			start := time.Now()
-
-			cids, err := r.store.ProvideCIDs(batchSize)
-			if err != nil {
-				reprovideSleep = time.Minute
-				r.log.Error("failed to fetch CIDs to provide", zap.Error(err))
-				break
-			} else if len(cids) == 0 {
-				reprovideSleep = 10 * time.Minute
-				r.log.Debug("reprovide complete")
-				break
-			}
-
-			rem := time.Until(cids[0].LastAnnouncement.Add(interval))
-			if rem > 0 && !triggered {
-				reprovideSleep = rem
-				r.log.Debug("reprovide complete")
-				break
-			}
-
-			announced := make([]cid.Cid, 0, len(cids))
-			keys := make([]multihash.Multihash, 0, len(cids))
-			// include a slight buffer for CIDs that are about to expire
-			// so they will be provided as one batch
-			buffer := interval / 10
-			minAnnouncement := time.Now().Add(-(interval - buffer))
-			for _, c := range cids {
-				// only provide CIDs that have not been provided within the
-				// last interval
-				if c.LastAnnouncement.After(minAnnouncement) {
-					break
-				}
-				keys = append(keys, c.CID.Hash())
-				announced = append(announced, c.CID)
-			}
-
-			if err := doProvide(ctx, keys); err != nil {
-				reprovideSleep = time.Minute
-				r.log.Error("failed to provide CIDs", zap.Error(err))
-				break
-			} else if err := r.store.SetLastAnnouncement(announced, time.Now()); err != nil {
-				reprovideSleep = time.Minute
-				r.log.Error("failed to update last announcement time", zap.Error(err))
-				break
-			}
-			r.log.Debug("provided CIDs", zap.Int("count", len(announced)), zap.Duration("elapsed", time.Since(start)))
 		}
 	}
+}
+
+func (r *Reprovider) performProvide(ctx context.Context, interval, timeout time.Duration, batchSize int) time.Duration {
+	doProvide := func(ctx context.Context, keys []multihash.Multihash) error {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		return r.provider.ProvideMany(ctx, keys)
+	}
+
+	reprovideSleep := 10 * time.Minute // Default sleep time if no CIDs to provide
+
+	start := time.Now()
+
+	cids, err := r.store.ProvideCIDs(batchSize)
+	if err != nil {
+		r.log.Error("failed to fetch CIDs to provide", zap.Error(err))
+		return time.Minute // Return a shorter sleep time on error
+	}
+
+	if len(cids) == 0 {
+		r.log.Debug("no CIDs to provide")
+		return reprovideSleep
+	}
+
+	rem := time.Until(cids[0].LastAnnouncement.Add(interval))
+	if rem > 0 {
+		r.log.Debug("waiting for next provide interval")
+		return rem
+	}
+
+	announced := make([]cid.Cid, 0, len(cids))
+	keys := make([]multihash.Multihash, 0, len(cids))
+	buffer := interval / 10
+	minAnnouncement := time.Now().Add(-(interval - buffer))
+	for _, c := range cids {
+		if c.LastAnnouncement.After(minAnnouncement) {
+			break
+		}
+		keys = append(keys, c.CID.Hash())
+		announced = append(announced, c.CID)
+	}
+
+	if err := doProvide(ctx, keys); err != nil {
+		r.log.Error("failed to provide CIDs", zap.Error(err))
+		return time.Minute // Return a shorter sleep time on error
+	}
+
+	if err := r.store.SetLastAnnouncement(announced, time.Now()); err != nil {
+		r.log.Error("failed to update last announcement time", zap.Error(err))
+		return time.Minute // Return a shorter sleep time on error
+	}
+
+	r.log.Debug("provided CIDs", zap.Int("count", len(announced)), zap.Duration("elapsed", time.Since(start)))
+
+	// If we've provided all CIDs, wait for the full interval before checking again
+	if len(announced) < len(cids) {
+		return time.Until(cids[len(announced)].LastAnnouncement.Add(interval))
+	}
+
+	return interval
 }
 
 // NewReprovider creates a new reprovider.
 func NewReprovider(provider Provider, store ReprovideStore, log *zap.Logger) *Reprovider {
 	return &Reprovider{
-		provider:       provider,
-		store:          store,
-		log:            log,
-		triggerProvide: make(chan struct{}, 1),
+		provider:             provider,
+		store:                store,
+		log:                  log,
+		triggerProvide:       make(chan struct{}, 1),
+		triggerDelayDuration: 30 * time.Second,
+		reprovideSleep:       time.Duration(0),
 	}
 }
